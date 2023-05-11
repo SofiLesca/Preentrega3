6e014107182ebdfe3bde0ae3ea127d8fe813d913
Sofia Lescaffette <sofilescaffette@gmail.com>
2023-05-10 22:35:34 -0300

    Agregado formulario para crear curso

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 
+from AppCoder.models import Estudiante, Curso
+
 def listar_estudiantes(request):
     contexto = {
-        "estudiantes/" : [
-            {"nombre": "Sofia","apellido":"Lescaffette"},
-            {"nombre": "Sofi","apellido":"Lescaffett"},
-            {"nombre": "Sofa","apellido":"Lescaffete"},
-            {"nombre": "Soia","apellido":"Lescafete"},
-        ] 
+        "estudiantes" : Estudiante.objects.all(),
     }
     http_response = render(
       request=request, 
@@ -18,16 +15,20 @@ def listar_estudiantes(request):
 
 def listar_cursos(request):
     contexto = {
-        "cursos/" :[
-            {"nombre": "Python","comision":"123"},
-            {"nombre": "CSS","comision":"111"},
-            {"nombre": "HTML","comision":"222"},
-            {"nombre": "C++","comision":"333"},
-        ] 
+        "cursos" : Curso.objects.all(),
     }
     http_response = render(
       request=request, 
     template_name='AppCoder/lista_cursos.html', 
+      context=contexto,
+  )
+    return http_response
+  
+def crear_curso(request):
+    contexto = {}
+    http_response = render(
+      request=request, 
+    template_name='AppCoder/formulario_1.html', 
       context=contexto,
   )
     return http_response
